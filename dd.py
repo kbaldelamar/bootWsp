1e165acd81811bb4e936944eba22dcafedf37941
kabaldelamar <kabaldelamar@genomaempresarial.com.co>
2024-08-20 15:55:27 -0500

    cambios

--- dd.py
+++ dd.py
@@ -1,75 +1,74 @@
-from flask import Flask, render_template , request , jsonify
+from flask import Flask,request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
-
+ 
 app = Flask(__name__)
-
+ 
+#Database configuration SQLITE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+ 
+#Log model table
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fecha_hora = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
     texto = db.Column(db.TEXT)
-    def __repr__(self):
-        return f'<Log {self.id}: {self.texto[:50]}...>'
-
-    def __str__(self):
-         return f"{self.fecha_hora.strftime('%Y-%m-%d %H:%M:%S')}: {self.texto}"
-
-
+ 
+#Crear la tabla si no existe
 with app.app_context():
     db.create_all()
-
-def ordenarfecha(registros):
-    return sorted(registros, key=lambda x: x.fecha_hora, reverse=True)
-
+ 
+#funcion para ordenar los registros por fecha y hora
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
+ 
 @app.route('/')
 def index():
+    #obtener registros de DB
     registros = Log.query.all()
-    registtrosorder=ordenarfecha(registros)
-    return render_template('index.html', registros=registtrosorder)
-
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html',registros=registros_ordenados)
+ 
 mensajes_log = []
-
-def agregar_log(texto):
+ 
+#funcion para agregar msjs y agregar en la DB
+def agregar_mensajes_log(texto):
     mensajes_log.append(texto)
+ 
+    #guardar mensaje en DB
     nuevo_registro = Log(texto=texto)
     db.session.add(nuevo_registro)
     db.session.commit()
-
-# agregar_log(json.dumps("asdasd"))
-
-
-TOKENKEVINCODE="KEVINCODE"
+ 
+#Token de verificacion para la  configuracion
+TOKEN_WACODE = "WACODE"
+ 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
-        challenge = verificarToken(request)
+        challenge = verificar_token(request)
         return challenge
     elif request.method == 'POST':
         response = recibir_mensajes(request)
         return response
-
-def verificarToken(req):
+ 
+def verificar_token(req):
     token = req.args.get('hub.verify_token')
     challenge = req.args.get('hub.challenge')
  
-    if challenge and token == TOKENKEVINCODE:
+    if challenge and token == TOKEN_WACODE:
         return challenge
     else:
         return jsonify({'error':'Token invalido'}),401
-
-def recibir_mensajes(req):
-    req=request.get_json()
-    agregar_log(req)
-    return jsonify({'message':'EVENT_RECEIVED'})
     
-
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+ 
+    return jsonify({'message':'EVENT_RECEIVED'})
+ 
+ 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-
-
-
+    app.run(host = '0.0.0.0', port=80, debug=True)
